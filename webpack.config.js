9e4f6eebdc99f014db208c1314d8f8e3db1757b5
Thomas Rademaker <thomas@sparrowtek.com>
2023-01-30 18:52:59 -0500

    WIP index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,3 @@
-// var path = require('path')
-
-// module.exports = {
-//     entry: "./index.js" ,
-//     output: {
-//         path:  path.resolve(__dirname, 'dist'),
-//         filename: "[name].bundle.js",
-//         library: "[name]",
-//         libraryTarget: "var"
-//     }
-// };
-
 const webpack = require('webpack');
 const path = require('path');
 
